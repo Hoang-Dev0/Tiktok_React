11d9e9fc43481f818031b94a7b005479cb3b43f7
NguyenPhucHoang <81604518+Hoang-Dev0@users.noreply.github.com>
2023-04-02 15:48:47 +0700

    [Add] create search component (#9)

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,17 +2,18 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem(props) {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://images.unsplash.com/photo-1677623466232-190170e88155?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
                 alt="avatar"
-            ></img>
+            ></Image>
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyen Phuc Hoang</span>
